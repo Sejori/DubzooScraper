d31eb1361e42b120fb13dfa8247d70261bd7429e
Seb Ringrose <sebringrose@gmail.com>
2019-03-14 01:37:13 +0000

    made soundcloud basic

--- socials/soundcloud.js
+++ socials/soundcloud.js
@@ -7,72 +7,18 @@ const keys = require('../config/keys.js')
 
 module.exports = {
   function: async function (handle) {
-    var clientID
-    var apiURI = "http://api.soundcloud.com"
-    var clientOrigin
+    // GET soundcloud user page HTML
+    const response = await fetch('https://www.soundcloud.com/' + handle)
+    let html = String(await response.text())
 
-    // start puppeteer headless browser
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.setViewport({width: 800, height: 600, deviceScaleFactor: 2})
+    let tracksStartIndex = html.indexOf('track_count') + 13
+    let tracksEndIndex = html.indexOf('uri') - 2
+    let trackCount = html.substring(tracksStartIndex, tracksEndIndex)
 
-    // tell browser to store clientID upon request (this is dank, supply our scrapes Soundcloud <3)
-    page.on('request', async (request) => {
-      if (await request._resourceType.match('xhr')) {
-        clientID = request._client._sessionId
-        clientOrigin = request._client._origin
-      }
-    })
+    let followersStartIndex = html.indexOf('followers_count') + 17
+    let followersEndIndex = html.indexOf('followings_count') - 2
+    let followerCount = html.substring(followersStartIndex, followersEndIndex)
 
-    // tell browser to GET instagram user page (this is just to get a client id)
-    const response = await page.goto('https://www.soundcloud.com/' + handle)
-    console.log(response)
-
-    // Soundcloud API GET user & tracks
-    const socialResponse = await fetch('https://cors-anywhere.herokuapp.com/' + apiURI + '/users/' + handle + '?client_id=' + clientID, {
-      headers: {
-        'x-requested-with': 'XMLHttpRequest'
-      }
-    })
-    .catch(err => console.log(err))
-    const socialJSON = await socialResponse.json()
-    console.log("------------- SOCIAL JSON -------------" + socialJSON)
-
-    // store metrics
-    let userSocialID = socialJSON.id
-    let userSocialFollowers = socialJSON.followers_count;
-    console.log(userSocialID, userSocialFollowers);
-
-    // Soundcloud API GET tracks
-    const tracksResponse = await fetch('https://cors-anywhere.herokuapp.com/' + apiURI + '/users/' + userSocialID + '/tracks?client_id=' + clientID)
-    const tracksJSON = await tracksResponse.json();
-    console.log("------------- JSON JSON -------------" + tracksJSON)
-
-
-    // stat totalling logic
-    let commentCount = []
-    let downloadCount = []
-    let favouriteCount = []
-    let playbackCount = []
-    let repostCount = []
-
-    tracksJSON.map((currElement, index) => {
-      commentCount[index] = currElement.comment_count;
-      downloadCount[index] = currElement.download_count;
-      favouriteCount[index] = currElement.favoritings_count;
-      playbackCount[index] = currElement.playback_count;
-      repostCount[index] = currElement.reposts_count;
-      return 'X';
-    });
-
-    commentCount = commentCount.reduce((a,b) => a + b, 0)
-    downloadCount = downloadCount.reduce((a,b) => a + b, 0)
-    favouriteCount = favouriteCount.reduce((a,b) => a + b, 0)
-    playbackCount = playbackCount.reduce((a,b) => a + b, 0)
-    repostCount = repostCount.reduce((a,b) => a + b, 0)
-
-    console.log("------------- RUNTIME -------------" + "Comments: " + commentCount, "Downloads: " + downloadCount, "Favourites: " + favouriteCount, "Plays: " + playbackCount, "Reposts: " + repostCount)
-
-    return  "Name: " + handle + "Comments: " + commentCount, "Downloads: " + downloadCount, "Favourites: " + favouriteCount, "Plays: " + playbackCount, "Reposts: " + repostCount
+    return("Track count: " + trackCount + " Follower count: " + followerCount)
   }
 }
