3e433e6e27a6946d8c2d1b081d60b346e6b08c0e
Seb Ringrose <sebringrose@gmail.com>
2019-03-28 13:52:39 +0000

    added prod config

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+// Dev config - Do not commit!
+module.exports = {
+  STRAPI_URI: process.env.STRAPI_URI,
+  STRAPI_USERNAME: process.env.STRAPI_USERNAME,
+  STRAPI_PASSWORD: process.env.STRAPI_PASSSWORD,
+  YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
+  SOUNDCLOUD_CLIENT_ID: process.env.SOUNDCLOUD_CLIENT_ID,
+  SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+  SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET
+}
