b64463104c0261204e1bdd658520f53c8bf46689
Seb Ringrose <sebringrose@gmail.com>
2019-04-23 14:48:28 +0100

    added no-sandbox flag to puppeteer

--- socials/instafans.js
+++ socials/instafans.js
@@ -7,7 +7,7 @@ module.exports = {
       var responses = []
 
       // start puppeteer headless browser
-      const browser = await puppeteer.launch()
+      const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']})
       const page = await browser.newPage()
       await page.setViewport({ width: 800, height: 600, deviceScaleFactor: 2 })
 
